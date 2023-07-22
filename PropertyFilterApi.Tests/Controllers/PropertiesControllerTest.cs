50b6a4ffe498428019687ab18dd0b7e2654c2f01
lily <s395135@sinyi.com.tw>
2023-07-22 11:44:11 +0800

    Controller單元測試: 情境2 Filt_MinPrice

--- PropertyFilterApi.Tests/Controllers/PropertiesControllerTest.cs
+++ PropertyFilterApi.Tests/Controllers/PropertiesControllerTest.cs
@@ -30,5 +30,29 @@ namespace PropertyFilterApi.Tests.Controllers
             result?.Value.Should().BeEquivalentTo(expectation);//集合用.BeEquivalentTo()
 
         }
+
+        [TestMethod]//testm快捷鍵
+        public void Filt_MinPrice()
+        {
+            var propertiesControllers = new PropertiesController();
+
+            var propertyRequest = new PropertyRequest()
+            {
+                MinPrice = 3000000000
+            };
+
+            var expectation = new List<PropertyResponse>()
+            {
+                new PropertyResponse("廣達科技大樓", 3000000000m, new Address { City = "台北市", District = "內湖區", Road = "基湖路", Number = "30號" }),
+                new PropertyResponse("富邦南港大樓", 3500000000m, new Address { City = "台北市", District = "南港區", Road = "經貿二路", Number = "188號" }),
+                new PropertyResponse("微風台北車站", 5000000000m, new Address { City = "台北市", District = "中正區", Road = "忠孝西路一段", Number = "49號" }),
+            };
+
+            var response = propertiesControllers.Get(propertyRequest);
+            var result = response as OkObjectResult;
+
+            result?.Value.Should().BeEquivalentTo(expectation);
+
+        }
     }
 }
